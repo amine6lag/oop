3e42281b6693447e2abf181ecc54f113cf138266
amine6lag <laggoune.am@gmail.com>
2025-11-27 18:25:40 +0100

    derniere modif

---
+++ opp.java
@@ -0,0 +1,12 @@
+impoort java.utile.Scanner
+
+public class game{
+    private int card;
+    private int player;
+    private int deck;
+    public game (int card,int player, int deck){
+         this.card=card;
+         this.player=player;
+         this.deck=deck;
+    }
+}
